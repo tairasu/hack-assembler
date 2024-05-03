aece8106ee3803024929364339aaab5a864e8dba
aomizu <marco.tylus24@gmail.com>
2024-05-03 23:53:26 +0900

    restructured and added CLI usage

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,47 @@ use std::collections::HashMap;
 
 fn main() {
 
-    let file = get_file();
+    //CLI ARGUMENTS
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        println!("Usage: ./rust-assembler <filepath>");
+        std::process::exit(1);
+    }
+
+    //GET FILE
+    let file = get_file(&args[1]);
+    let output = parse(&file);
+    std::fs::write(&args[1].replace(".asm", ".hack"), output).unwrap();
+
+}
+
+
+fn get_file(filepath: &str) -> String {
+
+        //READ FILE AND REMOVE COMMENTS
+
+        let file = std::fs::read_to_string(filepath).unwrap();
+        let lines = file.lines();
+        let mut output = String::new();
+        for line in lines {
+            let line = line.trim();
+            if line.is_empty() {
+                continue;
+            }
+            if line.starts_with("//") {
+                continue;
+            }
+            let line = line.split("//").next().unwrap().trim();
+            output.push_str(line);
+            output.push('\n');
+        }
+        output
+}
+
+fn parse(file: &str) -> String {
+    
     let mut output: String = String::new();
     let mut custom_symbols: HashMap<String, i16> = HashMap::new();
-
 
     //PREDEFINED SYMBOLS
     let predefined_symbols: [(&str, i32); 23] = [
@@ -33,8 +70,7 @@ fn main() {
         ("SCREEN", 16384),
         ("KBD", 24576),
     ];
-
-
+    
     //FIRST PASS
     //check if L instruction
     let lines = file.lines();
@@ -86,79 +122,56 @@ fn main() {
                     output.push('\n');
                 }
             }
-
-            
             
         } else {
 
             //C instruction
             let mut binary:String = String::from("111");
 
-            //comp
-            match line.split("=").nth(1) {
-                Some("0") => binary.push_str("0101010"),
-                Some("1") => binary.push_str("0111111"),
-                Some("-1") => binary.push_str("0111010"),
-                Some("D") => binary.push_str("0001100"),
-                Some("A") => binary.push_str("0110000"),
-                Some("M") => binary.push_str("1110000"),
-                Some("!D") => binary.push_str("0001101"),
-                Some("!A") => binary.push_str("0110001"),
-                Some("!M") => binary.push_str("1110001"),
-                Some("-D") => binary.push_str("0001111"),
-                Some("-A") => binary.push_str("0110011"),
-                Some("-M") => binary.push_str("1110011"),
-                Some("D+1") => binary.push_str("0011111"),
-                Some("A+1") => binary.push_str("0110111"),
-                Some("M+1") => binary.push_str("1110111"),
-                Some("D-1") => binary.push_str("0001110"),
-                Some("A-1") => binary.push_str("0110010"),
-                Some("M-1") => binary.push_str("1110010"),
-                Some("D+A") => binary.push_str("0000010"),
-                Some("D+M") => binary.push_str("1000010"),
-                Some("D-A") => binary.push_str("0010011"),
-                Some("D-M") => binary.push_str("1010011"),
-                Some("A-D") => binary.push_str("0000111"),
-                Some("M-D") => binary.push_str("1000111"),
-                Some("D&A") => binary.push_str("0000000"),
-                Some("D&M") => binary.push_str("1000000"),
-                Some("D|A") => binary.push_str("0010101"),
-                Some("D|M") => binary.push_str("1010101"),
-                _ => ()
-            }
+        // Determine the `comp` part of the instruction
+        let comp = if line.contains('=') {
+            // extract the part after '='
+            line.split('=').nth(1).unwrap_or("")
+        } else if line.contains(';') {
+            // extract the part before ';'
+            line.split(';').nth(0).unwrap_or("")
+        } else {
+            // Default case
+            ""
+        };
 
-            //comp for jump
-            match line.split(";").nth(0) {
-                Some("0") => binary.push_str("0101010"),
-                Some("1") => binary.push_str("0111111"),
-                Some("-1") => binary.push_str("0111010"),
-                Some("D") => binary.push_str("0001100"),
-                Some("A") => binary.push_str("0110000"),
-                Some("M") => binary.push_str("1110000"),
-                Some("!D") => binary.push_str("0001101"),
-                Some("!A") => binary.push_str("0110001"),
-                Some("!M") => binary.push_str("1110001"),
-                Some("-D") => binary.push_str("0001111"),
-                Some("-A") => binary.push_str("0110011"),
-                Some("-M") => binary.push_str("1110011"),
-                Some("D+1") => binary.push_str("0011111"),
-                Some("A+1") => binary.push_str("0110111"),
-                Some("M+1") => binary.push_str("1110111"),
-                Some("D-1") => binary.push_str("0001110"),
-                Some("A-1") => binary.push_str("0110010"),
-                Some("M-1") => binary.push_str("1110010"),
-                Some("D+A") => binary.push_str("0000010"),
-                Some("D+M") => binary.push_str("1000010"),
-                Some("D-A") => binary.push_str("0010011"),
-                Some("D-M") => binary.push_str("1010011"),
-                Some("A-D") => binary.push_str("0000111"),
-                Some("M-D") => binary.push_str("1000111"),
-                Some("D&A") => binary.push_str("0000000"),
-                Some("D&M") => binary.push_str("1000000"),
-                Some("D|A") => binary.push_str("0010101"),
-                Some("D|M") => binary.push_str("1010101"),
-                _ => ()
-            }
+        // Use a single `match` statement for the `comp` part
+        match comp {
+            "0" => binary.push_str("0101010"),
+            "1" => binary.push_str("0111111"),
+            "-1" => binary.push_str("0111010"),
+            "D" => binary.push_str("0001100"),
+            "A" => binary.push_str("0110000"),
+            "M" => binary.push_str("1110000"),
+            "!D" => binary.push_str("0001101"),
+            "!A" => binary.push_str("0110001"),
+            "!M" => binary.push_str("1110001"),
+            "-D" => binary.push_str("0001111"),
+            "-A" => binary.push_str("0110011"),
+            "-M" => binary.push_str("1110011"),
+            "D+1" => binary.push_str("0011111"),
+            "A+1" => binary.push_str("0110111"),
+            "M+1" => binary.push_str("1110111"),
+            "D-1" => binary.push_str("0001110"),
+            "A-1" => binary.push_str("0110010"),
+            "M-1" => binary.push_str("1110010"),
+            "D+A" => binary.push_str("0000010"),
+            "D+M" => binary.push_str("1000010"),
+            "D-A" => binary.push_str("0010011"),
+            "D-M" => binary.push_str("1010011"),
+            "A-D" => binary.push_str("0000111"),
+            "M-D" => binary.push_str("1000111"),
+            "D&A" => binary.push_str("0000000"),
+            "D&M" => binary.push_str("1000000"),
+            "D|A" => binary.push_str("0010101"),
+            "D|M" => binary.push_str("1010101"),
+            _ => (),
+        }
 
             //dest
             match line.split("=").nth(0) {
@@ -195,45 +208,5 @@ fn main() {
     //trim last \n
     output.pop();
     println!("{}", output);
-
-    // println!("{:?}", predefined_symbols);
-    // println!("{:?}", custom_symbols);
-
-    //validate output that each line is 16 bits
-    // let lines = output.lines();
-    // for line in lines {
-    //     if line.len() != 16 {
-    //         panic!("Error: Syntax error in input file. Each line should be 16 bits long.");
-    //     }
-    // }
-
-    //write to file with the .hack extension
-    std::fs::write("/Users/marco/VSCode/nand2tetris/hack-assembler/6/rect/Rect.hack", output).unwrap();
-
+    output
 }
-
-
-fn get_file() -> String {
-
-        //READ FILE AND REMOVE COMMENTS
-
-        let path = "/Users/marco/VSCode/nand2tetris/hack-assembler/6/rect/Rect.asm";
-        let file = std::fs::read_to_string(path).unwrap();
-        let lines = file.lines();
-        let mut output = String::new();
-        for line in lines {
-            let line = line.trim();
-            if line.is_empty() {
-                continue;
-            }
-            if line.starts_with("//") {
-                continue;
-            }
-            let line = line.split("//").next().unwrap().trim();
-            output.push_str(line);
-            output.push('\n');
-        }
-        println!("{}", output);
-        output
-}
-
